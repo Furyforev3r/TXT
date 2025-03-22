5f249d278ad03bd1e4d28a1655cfc8bfc945eee0
FuryForever <messiasfuryforever@gmail.com>
2025-03-22 19:19:30 -0300

    feat: home page placeholder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,13 +8,23 @@
 
 <main>
     <img src={Logo} alt="Txt." />
+
+    <div class="description">
+        <div>
+            <h1>Welcome to TXT.</h1>
+            <p>Your favorite platform for smooth and efficient communication.</p>
+        </div>
+        <button>Get Started</button>
+    </div>
 </main>
 
 <style>
     main {
-        display: grid;
-        place-items: center;
-        height: 100vh;
+        height: 100dvh;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        align-items: center;
     }
 
     img {
@@ -22,12 +32,38 @@
         cursor: pointer;
         width: 250px;
         height: 250px;
-        transition: box-shadow 0.3s;
+        transition: scale 0.3s;
     }
 
     img:hover {
-        -webkit-box-shadow: 0px 0px 20px 0px var(--button-background);
-        -moz-box-shadow: 0px 0px 20px 0px var(--button-background);
-        box-shadow: 0px 0px 20px 0px var(--button-background);
+        scale: 103%;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .description {
+        border-radius: 0.8rem;
+        gap: 0.8rem;
+    }
+
+    .description button {
+        align-self: center;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        color: var(--text-alternative);
+        padding: 1.2rem;
+        background: var(--blue-primary-color);
+        border: none;
+        border-radius: 0.8rem;
+        transition: 0.3s background;
+    }
+
+    .description button:hover {
+        background: var(--blue-secondary-color);
     }
 </style>
