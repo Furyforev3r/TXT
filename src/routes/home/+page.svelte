733db79542b300c389b398407c9ba53062c106f7
FuryForever <messiasfuryforever@gmail.com>
2025-03-22 20:20:11 -0300

    chore: home page title changed

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,6 +2,10 @@
     import Sidebar from "$lib/components/interface/sidebar.svelte"
 </script>
 
+<svelte:head>
+    <title>TXT - Home</title>
+</svelte:head>
+
 <div class="home">
     <Sidebar />
 </div>
